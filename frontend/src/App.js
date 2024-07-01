371d425eaa0b9fcdf181ed787b6c56c3c2e2a0a8
mishra06 <mishra.vibhakar7262@gmail.com>
2024-07-01 12:24:03 +0530

    frontend ui almost completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import Home from "./pages/Home/Home";
+// import SignUp from "./pages/Signup/SignUp";
+// import Login from "./pages/login/Login";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      {/* <Login/> */}
+      {/* <SignUp/> */}
+      <Home/>
     </div>
   );
 }
