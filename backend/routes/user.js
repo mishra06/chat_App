3cdca4a383f06b0099cbf91236f1c398a417da4a
mishra06 <mishra.vibhakar7262@gmail.com>
2024-07-08 16:09:05 +0530

    changes

--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authantation = require('../middleware/authantation');
-const getUserControllerForSidebar = require('../controllers/user');
-const router = express.Router();
-
-router.get("/",authantation,getUserControllerForSidebar.getUser);
-
-module.exports = router;
--- frontend/src/redux/store/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import messageSlice from "../sllice/Slice";
-const store = configureStore({
-    reducer : {
-        messageSlice,
-    }
-})
-
-export default store;
