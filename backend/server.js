b1c2dcc0517088d27d7c74037b0731f32011b239
mishra06 <mishra.vibhakar7262@gmail.com>
2024-07-08 14:36:34 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,12 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 dotenv.config();
-const PORT = process.env.PORT;
-// const server = express();
 const { app , server } =require("./socket/socket");
+const PORT = process.env.PORT;
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 const authRouter = require("./routes/auth");
 const messageRouter = require("./routes/message");
 const userRouter = require("./routes/user");
@@ -18,8 +20,12 @@ mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("Database connected succesfully"))
 .catch((err)=> console.log(`Error in DB connection ${err}`));
 
-const corsOptions = {
-    origin:["http://localhost:3000","https://chat-app-theta-smoky.vercel.app"],
+  // const corsOptions = {
+  //   origin:["http://localhost:3000","https://chat-app-theta-smoky.vercel.app"],
+  //   credentials:true
+  // }
+  const corsOptions = {
+    origin:"http://localhost:3000",
     credentials:true
   }
   
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,15 +1,16 @@
 const { Server } = require("socket.io");
 const http = require("http");
-const express = require("express");;
+const express = require("express");
 
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:["http://localhost:3000","https://chat-app-theta-smoky.vercel.app"],
+        // origin:["http://localhost:3000","https://chat-app-theta-smoky.vercel.app"],
+        origin:"http://localhost:3000",
         methods:["GET","POST"],
-        credentials:true
+        // credentials:true
     },
 });
 
